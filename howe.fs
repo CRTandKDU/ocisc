0b1269028a5d781bd7fa53d8b01a63aed02b18f9
CRTandKDU <jeanmariechauvet@gmail.com>
2023-04-17 11:55:30 +0200

    Fixed strings. Added talloc, tvar, tlabel:

--- howe.fs
+++ howe.fs
@@ -1,3 +1,4 @@
+( -*- mode: forth; -*-  )
 ( SUBLEQ image constants )
 2 CONSTANT =cell
 4000 CONSTANT size
@@ -15,10 +16,10 @@ VARIABLE tdp 0 tdp !
 : there tdp @ ;
 : tc! tflash + c! ; : tc@ tflash + c@ ;
 : t! tflash + !  ; : t@ tflash + @ ;
-: t, there t! tcell tdp +! ;
+: t, there t! tcell tdp +! ; : tc, there tc! 1 tdp +! ;
 : talign there 1 AND tdp +! ;
-: ts" $STRING t, there BEGIN KEY DUP 34 <> WHILE OVER tc! 1+ REPEAT DROP DUP 1 AND + tdp ! $STRING t, ;
-( Tentative SUBLEQ assembler )
+: ts" there BEGIN KEY DUP 34 <> WHILE OVER tc! 1+ REPEAT DROP DUP 1 AND + tdp !  ;
+( Tentative SUBLEQ assembler instruction set )
 : Z 0 t, ;
 : NADDR there 2/ 1+ t, ;
 : HALT Z Z -1 t, ;
@@ -33,11 +34,16 @@ VARIABLE tdp 0 tdp !
 : ILOAD there 2/ 3 4 * 3 + + MOV 0 SWAP MOV ;
 : IJMP there 2/ 14 + MOV Z Z NADDR ;
 : ISTORE SWAP >R there 2/ 36 + 2DUP MOV 2DUP 1+ MOV 7 + MOV R> 0 MOV ;
+( Tentative SUBLEQ assembler macros )
+( Alloc in target )
+: talloc tdp @ SWAP tdp +! ;
+( Variable in target )
+: tvar tcell talloc CREATE DOCOL , ' LIT , , ' EXIT , ;
+( Label in target. If used in SUBLEQ instr has to be halved )
+: tlabel: there 2/ CREATE DOCOL , ' LIT , , ' EXIT , ; 
 ( Debugging help )
-: ttype 2+
-    BEGIN DUP tc@ EMIT 1+ DUP tc@ EMIT 1+ DUP t@ $STRING - 0=
-    UNTIL CR ;
-: t. DUP $STRING - 0 <> IF . CR ELSE DROP ttype THEN ;
+: t. . CR ;
 : tdump 0 BEGIN DUP t@ t. 2+ DUP there - 0= UNTIL DROP ;
+: tmem 0 BEGIN DUP t@ . 9 EMIT DUP tc@ DUP . SPACE EMIT 9 EMIT 1+ DUP tc@ DUP . SPACE EMIT CR 1+ DUP there - 0= UNTIL DROP ;
 : trace IF s" out.slq" FOPEN ELSE FCLOSE THEN ;
 : timage 1 trace tdump 0 trace ;
